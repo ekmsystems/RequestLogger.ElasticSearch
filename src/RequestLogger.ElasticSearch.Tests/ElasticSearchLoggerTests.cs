228d4a4da826bf8891877ea3867e045b4c246694
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-04-04 16:47:54 +0100

    Added the test project.

---
+++ src/RequestLogger.ElasticSearch.Tests/ElasticSearchLoggerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace RequestLogger.ElasticSearch.Tests
+{
+    [TestFixture]
+    [Parallelizable]
+    public class ElasticSearchLoggerTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _logger = new ElasticSearchLogger();
+        }
+
+        private IRequestLogger _logger;
+
+        [Test]
+        public void Log_ShouldThrow_NotImplementedException()
+        {
+            var request = new RequestData();
+            var response = new ResponseData();
+            Assert.Throws<NotImplementedException>(() => _logger.Log(request, response));
+        }
+
+        [Test]
+        public void LogError_ShouldThrow_NotImplementedException()
+        {
+            var request = new RequestData();
+            var response = new ResponseData();
+            var ex = new Exception();
+            Assert.Throws<NotImplementedException>(() => _logger.LogError(request, response, ex));
+        }
+    }
+}
