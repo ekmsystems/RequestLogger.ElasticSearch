ce0d7e8baab7e488616673c369c212a25e2349f6
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-04-20 15:21:03 +0100

    Added example projects.

---
+++ src/Examples/AspNetExample/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using RequestLogger.ElasticSearch;
+using RequestLogger.Web;
+
+namespace AspNetExample
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private static readonly RequestLoggerModule RequestLoggerModule = CreateRequestLoggerModule();
+
+        public override void Init()
+        {
+            base.Init();
+
+            RequestLoggerModule.Init(this);
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private static RequestLoggerModule CreateRequestLoggerModule()
+        {
+            var requestLogger = new ElasticSearchLogger();
+            return new RequestLoggerModule(requestLogger);
+        }
+    }
+}
---
+++ src/Examples/OwinExample/App_Start/Startup.Logging.cs
@@ -0,0 +1,22 @@
+﻿using Owin;
+using RequestLogger.ElasticSearch;
+using RequestLogger.Owin;
+
+namespace OwinExample
+{
+    public partial class Startup
+    {
+        private static void ConfigureLogging(IAppBuilder app)
+        {
+            var requestLogger = new ElasticSearchLogger();
+
+            app.UseRequestLoggerMiddleware(requestLogger);
+
+            app.Run(ctx =>
+            {
+                ctx.Response.ContentType = "text/plain";
+                return ctx.Response.WriteAsync("Hello World");
+            });
+        }
+    }
+}
